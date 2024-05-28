6fcd010a80e678b2f50d414c0135ce505b200c9e
vasanthabalanm <vasanthabalanm.kanini@gmail.com>
2024-05-28 16:58:01 +0530

    code changes updated

--- client/OnlineFoodOrder/src/app/Constant/CommonEndpoint.ts
+++ client/OnlineFoodOrder/src/app/Constant/CommonEndpoint.ts
@@ -3,7 +3,7 @@ export const CommonEndpoint = {
     pendingUserEndpoint:'https://localhost:7012/api/PendingUser',
     addHotel:'https://localhost:7012/api/AddHotel',
     addBranch:'https://localhost:7012/api/AddBranch',
-    addmenu:'https://localhost:7012/api/AddMenu',
+    addmenu:'https://localhost:7012/api/Menu',
     userMenu:'https://localhost:7012/api/UserViewMenu',
     pendingOrder:'https://localhost:7012/api/Order'
 }
